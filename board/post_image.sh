184650a9e1acbbd3c8f351f97cff7eb294ebac7c
Andrew Orals <36123025+nd-0r@users.noreply.github.com>
2024-07-16 08:36:20 +0000

    Make ext4 image

---
+++ board/post_image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+ENV_TXT="$BR2_EXTERNAL"/env/env.txt
+OUTPUT="$BR2_EXTERNAL"/output
+EXT4_IMAGE="$OUTPUT"/data.ext4
+MOUNTPOINT="$BASE_DIR"/superduperbird_data_mnt
+
+cleanup() {
+  umount "$MOUNTPOINT" || /bin/true
+  [ -f "$OUTPUT"/"$ENV_TXT" ] && rm "$OUTPUT"/"$ENV_TXT"
+  [ -f "$EXT4_IMAGE" ] && rm "$EXT4_IMAGE"
+  return 1
+}
+
+[ -f "$OUTPUT"/"$ENV_TXT" ] && mv -f "$OUTPUT"/"$ENV_TXT" "$OUTPUT"/"$ENV_TXT".backup
+[ -f "$EXT4_IMAGE" ] && mv -f "$EXT4_IMAGE" "$EXT4_IMAGE".backup
+
+trap cleanup INT
+
+# Based on the size of the data dump, 2G appears to be safe
+dd if=/dev/zero of="$EXT4_IMAGE" bs=1M count=2048 || cleanup
+mkfs.ext4 "$EXT4_IMAGE" || cleanup
+mkdir "$MOUNTPOINT" || /bin/true
+sudo mount -t ext4 -o loop "$EXT4_IMAGE" "$MOUNTPOINT" || cleanup
+sudo tar -xf "$BINARIES_DIR"/rootfs.tar -C "$MOUNTPOINT" || cleanup
+sudo umount "$MOUNTPOINT"
+
+cp "$ENV_TXT" "$OUTPUT"/"$(basename $ENV_TXT)" || cleanup
